95f4f17220d6e8dbf02596a2ebca03752378bc14
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-01 16:20:51 +0530

    practice

---
+++ sumofnumbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+	int main() {
+		 int arr[10],sum=0;
+		 printf("Enter two digit numbers\n");
+	     for( int i=0;i<10;i++ ) {
+		 scanf("%d",&arr[i] );
+		}
+	  for(int i=0;i<10;i++ ) {
+		if( arr[i]>0 ) {
+			if( sum <= 999 )
+			sum = sum + arr[i];
+		    }
+             }
+ 		printf("sum is %d\n",sum);
+		return 0;
+		}
+
